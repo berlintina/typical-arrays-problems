e1ebe12437d059f7ca713331f95055ae4b9d0381
Berlintina <val@berlinjohn.de>
2020-02-25 22:05:47 +0100

    fix:add min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || !array.length) return 0;
+
+    array.sort(function(a, b) {
+        return a - b;
+    })
+    return array[0];
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || !array.length) return 0;
+
+    array.sort(function(a, b) {
+        return a - b;
+    })
+    return array[array.length - 1];
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || !array.length) return 0;
+
+    var summ = 0;
+    array.forEach(function(value) {
+        summ += value;
+    });
+    return summ / array.length;
+
 }
